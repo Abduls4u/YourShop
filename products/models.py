0220c0ddfdfa2d3193317b17947c2eba30b529d2
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-12-24 15:35:10 +0100

    Create app models

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(null=True)
+
+    def __str__(self) -> str:
+        return (self.name)
