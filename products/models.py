c8c66076481a60842b245054ba93e81685a6852d
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2024-01-04 12:22:16 +0100

    Update: create custom user model, add price and image fields to product model

--- products/models.py
+++ products/models.py
@@ -1,14 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    storeName = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+
 class Product(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
+    price = models.IntegerField()
     description = models.TextField()
-    image = models.ImageField(null=True)
+    image = models.ImageField(null=True,)
 
     def __str__(self) -> str:
         return (self.name)
