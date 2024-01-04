9287b912f2f4523ca195d11ae0c9186f1d537bdd
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2024-01-04 13:58:18 +0100

    Update YourShop Modified:     YourShop/urls.py =>             Add static url and root to path
        products/admin.py =>
                Register the CustomUser model
    
        products/forms.py =>
                Add storeName field to RegistrationForm
                Update ProductForm with price field and data validation methods
    
        products/templates/products/register.html =>
                Rename title to signup
                Add crispy form for user registration
    
        products/urls.py =>
                Add storefront, about, and logout
    
        products/views.py =>
                Update add product logic
                Add delete product functionality
                Add pagination to storefront
    
    New file:
        products/templates/products/storefront.html =>
                Rename view_user_products
    
    Change static folder path:
        products/static/products/css/layout.css -> static/products/css/layout.css

--- products/forms.py
+++ products/forms.py
@@ -1,19 +1,60 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .models import Product, CustomUser
 
-from products.models import Product
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    storeName = forms.CharField(max_length=15, required=True)
 
     class Meta:
-        model = User
-        fields = ['username', 'email','password1', 'password2']
-
+        model = CustomUser
+        fields = ['username', 'email', 'storeName', 'password1', 'password2']
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'image']
+        fields = ['name', 'price', 'description', 'image']
         widgets = {'user': forms.HiddenInput()}
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_class = 'form-horizontal'  # Bootstrap form layout
+        self.helper.label_class = 'col-lg-2'  # Bootstrap column size for labels
+        self.helper.field_class = 'col-lg-10'  # Bootstrap column size for form fields
+        self.helper.layout = Layout(
+            Field('name', css_class='form-control', placeholder='Enter product name'),
+            Field('price', css_class='form-control', placeholder='Enter product price'),
+            Field('description', css_class='form-control', placeholder='Enter product description'),
+            Field('image', css_class='form-control', placeholder='Enter product image URL'),
+            'user',  # Hidden field
+            Submit('submit', 'Save product', css_class='btn btn-success')
+        )
+
+    def clean_name(self):
+        data = self.cleaned_data['name']
+        if len(data) < 4:
+            raise forms.ValidationError("Name must be at least 4 characters.")
+        elif data[0] == ' ':
+            raise forms.ValidationError("No space at the beginning of name and it must be at least 4 characters.")
+        return data
+
+    def clean_price(self):
+        data = self.cleaned_data['price']
+        if data <= 0:
+            raise forms.ValidationError("Price must be greater than zero.")
+        return data
+    
+    def clean_description(self):
+        data = self.cleaned_data['description']
+        if len(data) < 10:
+            raise forms.ValidationError("product description must be at least 3 characters.")
+        elif data[0] == ' ':
+            raise forms.ValidationError("No space at the beginning of description and it must be at least 3 characters.")
+        return data
--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse
 from django.contrib.auth import login
-from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, CustomUser
 from .forms import ProductForm, RegistrationForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # Create your views here.
 def register(request):
@@ -25,34 +25,60 @@ def register(request):
 
 @login_required(login_url='login')
 def manage_products(request):
-    user_products = Product.objects.filter(user=request.user)
+    form = ProductForm()
     if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.user = request.user
-            product.save()
-            return (redirect(reverse('manage_products')))
-    else:
-            form = ProductForm()
+        action = request.POST.get('action', None)
+        if action == 'add_product':
+            # Handle product creation
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                product = form.save(commit=False)
+                product.user = request.user
+                product.save()
+                return redirect(reverse('manage_products'))
+
+        elif action == 'delete_product':
+            # Handle product deletion
+            product_id = request.POST.get('delete_product_id', None)
+            if product_id:
+                product = get_object_or_404(Product, id=product_id, user=request.user)
+                product.delete()
+                return redirect(reverse('manage_products'))
+
+    # Retrieve and display existing products
+    products = Product.objects.filter(user=request.user)
+    storeRelUrl = reverse('storefront', args=[request.user.id])
+    storeAbsUrl = request.build_absolute_uri(storeRelUrl)
     context = {
         'user_name': request.user.username,
-        'user_products': user_products,
         'form': form,
-        'user_id': request.user.id,
+        'products': products,
+        'storeLink': storeAbsUrl,
     }
-    return (render(request, 'products/manage_products.html', context))
+    return render(request, 'products/manage_products.html', context)
 
-def view_user_products(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    user_products = Product.objects.filter(user=user)
+def storefront(request, user_id):
+    user = get_object_or_404(CustomUser, id=user_id)
+    user_products = Product.objects.filter(user=user).order_by('id')
+
+    # Pagination
+    page = request.GET.get('page', 1)
+    paginator = Paginator(user_products, 9)
+    try:
+        user_products_paginated = paginator.page(page)
+    except PageNotAnInteger:
+        user_products_paginated = paginator.page(1)
+    except EmptyPage:
+        user_products_paginated = paginator.page(paginator.num_pages)
+
     context = {
         'user': user,
-        'user_products': user_products
+        'user_products': user_products_paginated,
+        'valid_user': request.user,
     }
-    return (render(request, 'products/view_user_products.html', context))
+    return render(request, 'products/storefront.html', context)
 
-def index(request):
+
+def about(request):
     context = {}
-    return (render(request, 'products/layout.html', context))
-
+    return (render(request, 'products/about.html', context))
